7f7d2d578700aa06e20f513d013f9c3729281012
Hamza Laabidi <99836375+laabidihamza@users.noreply.github.com>
2023-03-09 14:44:33 +0100

    done with login page layout

--- app/src/main/java/com/example/penpal/Pagelogin.kt
+++ app/src/main/java/com/example/penpal/Pagelogin.kt
@@ -2,10 +2,17 @@ package com.example.penpal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class Pagelogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pagelogin)
+
+        val google_connect = findViewById<Button>(R.id.google_connect)
+            google_connect.setOnClickListener {
+                Toast.makeText(this,"BUTTONS", "User tapped the Supabutton",Toast.LENGTH_LONG).show()
+            }
     }
 }
