155bacd7f9d1debddec3a323d9cdd2ddfa4c5525
Hamza Laabidi <99836375+laabidihamza@users.noreply.github.com>
2023-03-20 23:23:53 +0100

    creating auth method

--- app/src/main/java/com/example/penpal/Homemenu.kt
+++ app/src/main/java/com/example/penpal/Homemenu.kt
@@ -2,10 +2,16 @@ package com.example.penpal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class Homemenu : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homemenu)
+
+        auth = FirebaseAuth.getInstance()
     }
 }
--- app/src/main/java/com/example/penpal/Pagelogin.kt
+++ app/src/main/java/com/example/penpal/Pagelogin.kt
@@ -1,72 +1,121 @@
 package com.example.penpal
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 
 class Pagelogin : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pagelogin)
+
+        auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+        val googleConnect = findViewById<Button>(R.id.google_connect)
+
+        googleConnect.setOnClickListener{
+            signInGoogle()
+        }
+
+        private fun signInGoogle(){
+            val signInIntent = googleSignInClient.signInIntent
+            luncher.lunch(signInIntent)
+        }
+
+        private val luncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+                    if(result.resultCode == Activity.RESULT_OK){
+                        val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                        handleResults(task)
+                    }
+            private fun handleResults(task: Task<GoogleSignInAccount>) {
+                if(task.isSuccessful){
+                    val account :GoogleSignInAccount? = task.result
+                    if(account !=null){
+                        updateUI(account)
+                    }
+                }else{
+                    Toast.makeText(this,task.exception.toString(),Toast.LENGTH_SHORT).show()
+                }
+            }
+            private fun updateUI(account: GoogleSignInAccount) {
+                val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+                auth.signInWithCredential(credential).addOnCompleteListener {
+                    if(it.isSuccessful){
+                        val intent : Intent = Intent(this,Homemenu::class.java)
+                        intent.putExtra("email",account.email)
+                        intent.putExtra("name",account.displayName)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(this,it.exception.toString(),Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
 
         val signup = findViewById<TextView>(R.id.signup)
         val login = findViewById<Button>(R.id.login_btn)
         val email = findViewById<EditText>(R.id.email)
         val pwd = findViewById<EditText>(R.id.password)
         val error = findViewById<TextView>(R.id.error)
+
         val correctemail = "abc@gmail.com"
         val correctpwd = "azerty"
+
+        signup.setOnClickListener {
+            Intent(this, PageSignIn::class.java).also {
+                startActivity(it)
+            }
+        }
 
         login.setOnClickListener {
             val txtemail = email.text.toString()
             val txtpwd = pwd.text.toString()
-            if (txtemail.trim().isEmpty() || txtpwd.trim().isEmpty()){
-             error.text="Please fill in the blank spaces"
-             error.visibility = View.VISIBLE }
-               else{ if (correctemail == txtemail && correctpwd == txtpwd) {
+            if (txtemail.trim().isEmpty() || txtpwd.trim().isEmpty()) {
+                error.text = "Please fill in the blank spaces"
+                error.visibility = View.VISIBLE
+            } else {
+                if (correctemail == txtemail && correctpwd == txtpwd) {
                     Intent(this, Homemenu::class.java).also {
-                        startActivity(it) } }
-            else{ error.text="The username or password provided in the request are invalid"
-                error.visibility =View.VISIBLE}}
+                        startActivity(it)
+                    }
+                } else {
+                    error.text = "The username or password provided in the request are invalid"
+                    error.visibility = View.VISIBLE
+                }
+            }
         }
-
-<<<<<<< HEAD
-        // Configure sign-in to request the user's ID, email address, and basic
-// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        // Configure sign-in to request the user's ID, email address, and basic
-// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestEmail()
-            .build()
-
-        // Build a GoogleSignInClient with the options specified by gso.
-        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        val account = GoogleSignIn.getLastSignedInAccount(this)
-
-        val google_connect = findViewById<Button>(R.id.google_connect)
-        google_connect.visibility = View.VISIBLE
-    }
-}
-        signup.setOnClickListener{
-            Intent(this, PageSignIn::class.java).also {
-                startActivity(it) } }
-    } }
-=======
-        signup.setOnClickListener{
-            Intent(this, PageSignIn::class.java).also {
-                startActivity(it) } }
     }
 }
 
 
 
 
->>>>>>> 031ab0c6809d08fa04eb8f2531a2537ed726c767
 
